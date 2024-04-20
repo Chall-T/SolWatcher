339e504a27743f6856f644aa23d1c1d65c831451
Chall-T <gdav476@gmail.com>
2024-04-20 14:06:41 +0200

    init

---
+++ src/handle_token.rs
@@ -0,0 +1,120 @@
+use async_recursion::async_recursion;
+use async_std::task;
+use reqwest::header;
+use reqwest::Client;
+use serde_json::Value;
+use std::any::Any;
+use std::any::TypeId;
+use std::error::Error;
+use std::time::Duration;
+
+use crate::utils::Logger;
+
+#[async_recursion]
+pub async fn get_transaction(
+    signature: &str,
+    encoding: &str,
+    http: &str,
+) -> Result<Value, Box<dyn Error>> {
+    let logger = Logger::new(String::from("Token handler"));
+    let mut headers = header::HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+
+    let client = Client::new();
+    let json_data = format!(
+        "
+    {{
+        \"jsonrpc\": \"2.0\",
+        \"id\": 1,
+        \"method\": \"getTransaction\",
+        \"params\": [
+            \"{signature}\",
+            {{
+                \"encoding\": \"{encoding}\",
+                \"maxSupportedTransactionVersion\": 0
+            }}
+        ]
+    }}"
+    );
+    let response = client
+        .post(http)
+        .headers(headers)
+        .body(json_data)
+        .send()
+        .await?;
+
+    let body = response.text().await?;
+    
+    let mut body_json: Value = serde_json::from_str(body.as_str()).expect("Failed to parse JSON");
+    logger.debug(format!("getTransaction [\"result\"] type: {:?}, is type string: {:?}", body_json["result"].type_id(), body_json["result"].type_id() == TypeId::of::<String>()));
+
+    if body_json["result"].type_id() == TypeId::of::<String>() {
+        
+        logger.debug(format!("Resending getTransaction request for \"{}\" signature", signature));
+        task::sleep(Duration::from_secs(1)).await;
+        body_json = get_transaction(signature, encoding, http).await.unwrap();
+    }
+
+    Ok(body_json)
+}
+
+pub fn get_instructions(json: Value) -> Vec<serde_json::Value> {
+    let mut instructions = Vec::new();
+    if let Some(raw_instructions) =
+        json["result"]["transaction"]["message"]["instructions"].as_array()
+    {
+        for raw_instruction in raw_instructions {
+            instructions.push(raw_instruction.clone());
+        }
+    }
+    instructions
+}
+
+pub fn get_instructions_with_program_id(json: Value, program_id: String) -> Vec<serde_json::Value> {
+    let mut filtred_instuctions = Vec::new();
+
+    let instructions = get_instructions(json);
+    if instructions.is_empty() {
+        let logger = Logger::new(String::from("Token handler"));
+        logger.error("No instructions found".to_string());
+        return filtred_instuctions;
+    }
+    for instruction in instructions {
+        if instruction["programId"].eq(&program_id) {
+            filtred_instuctions.push(instruction);
+        }
+    }
+    filtred_instuctions
+}
+pub fn get_tokens_info(
+    instruction: serde_json::Value,
+) -> (serde_json::Value, serde_json::Value, serde_json::Value) {
+    let accounts = &instruction["accounts"];
+    let pair = &accounts[4];
+    let token0 = &accounts[8];
+    let token1 = &accounts[9];
+    (token0.clone(), token1.clone(), pair.clone())
+}
+
+pub fn token_show_info_detailed(instructions: Vec<serde_json::Value>){
+    for instruction in instructions {
+        let tokens = get_tokens_info(instruction);
+        println!("Token0: {}", &tokens.0.as_str().unwrap().replace('\"', ""));
+        println!("Token1: {}", &tokens.1.as_str().unwrap().replace('\"', ""));
+        println!(
+            "LP Pair: {}",
+            &tokens.2.as_str().unwrap().replace('\"', "")
+        );
+        println!(
+            "Dex: https://dexscreener.com/solana/{}",
+            &tokens.2.as_str().unwrap().replace('\"', "")
+        );
+    }
+}
+pub fn token_show_info(instructions: Vec<serde_json::Value>){
+    let logger = Logger::new(String::from("Token handler"));
+    for instruction in instructions {
+        let tokens = get_tokens_info(instruction);
+        logger.log(format!("new pair found (Token: {} LP Pait: {})", &tokens.0.as_str().unwrap(), &tokens.2.as_str().unwrap()));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,87 @@
+use anyhow::Result;
+use dotenv::dotenv;
+use futures::prelude::*;
+use solana_client::{
+    nonblocking::pubsub_client::PubsubClient,
+    rpc_config::{RpcTransactionLogsConfig, RpcTransactionLogsFilter},
+    rpc_response::{Response, RpcLogsResponse},
+};
+use solana_sdk::commitment_config::CommitmentConfig;
+pub mod handle_token;
+pub mod utils;
+
+struct Configuration {
+    wss_url: String,
+    https_url: String,
+    log_instruction: String,
+    raydium_lpv4: String,
+}
+impl Configuration {
+    fn new() -> Self {
+        let wss_url = std::env::var("SOL_WSS")
+            .unwrap_or_else(|_| "wss://api.mainnet-beta.solana.com".to_string());
+        let https_url = std::env::var("SOL_HTTPS")
+            .unwrap_or_else(|_| "https://api.mainnet-beta.solana.com".to_string());
+        let log_instruction =
+            std::env::var("LOG_INSTRUCTION").unwrap_or_else(|_| "initialize2".to_string());
+        let raydium_lpv4 = std::env::var("RAYDIUM_LPV4")
+            .unwrap_or_else(|_| "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8".to_string());
+
+        Configuration {
+            wss_url,
+            https_url,
+            log_instruction,
+            raydium_lpv4,
+        }
+    }
+}
+lazy_static::lazy_static! {
+    static ref CONFIG: Configuration = Configuration::new();
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    let logger = utils::Logger::new("Setup".to_string());
+
+    logger.log(format!("Solana RPC websocket: {:?}", CONFIG.wss_url.as_str()));
+    logger.log(format!("Solana RPC http: {:?}", CONFIG.https_url.as_str()));
+    logger.log(format!("Log instruction: {:?}", CONFIG.log_instruction.as_str()));
+
+    let ws_client = PubsubClient::new(CONFIG.wss_url.as_str()).await?;
+    let (mut stream, _) = ws_client
+        .logs_subscribe(
+            RpcTransactionLogsFilter::Mentions(vec![CONFIG.raydium_lpv4.to_string()]),
+            RpcTransactionLogsConfig {
+                commitment: Some(CommitmentConfig::finalized()),
+            },
+        )
+        .await?;
+    
+    logger.log("Subscribed to Raydium Liquidity Pool".to_string());
+    loop {
+        let response: Response<RpcLogsResponse> = stream.next().await.unwrap();
+        process_message(response).await;
+    }
+}
+async fn process_message(response: Response<RpcLogsResponse>) {
+    let value = response.value;
+    for log in value.logs {
+        if !log.contains(CONFIG.log_instruction.as_str()) {
+            continue;
+        }
+        let signature_str = &value.signature;
+        get_tokens(&signature_str, CONFIG.raydium_lpv4.to_string()).await;
+    }
+}
+async fn get_tokens(sign: &str, program: String) {
+    let result = handle_token::get_transaction(sign, "jsonParsed", CONFIG.https_url.as_str())
+        .await
+        .unwrap();
+
+    let instructions = handle_token::get_instructions_with_program_id(result, program);
+    
+    
+    handle_token::token_show_info(instructions);
+
+}
