dc22f43de58ddb36403ad6018299af702e98871d
David <gdav476@gmail.com>
2025-02-05 15:25:49 +0100

    auto reconnection added

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use solana_client::{
     rpc_response::{Response, RpcLogsResponse},
 };
 use solana_sdk::commitment_config::CommitmentConfig;
+use tokio::time::sleep;
+use std::time::Duration;
 pub mod handle_token;
 pub mod utils;
 
@@ -39,6 +41,57 @@ lazy_static::lazy_static! {
     static ref CONFIG: Configuration = Configuration::new();
 }
 
+
+
+async fn start_subscriber() -> Result<()> {
+    let mut attempts = 0;
+    loop {
+        attempts += 1;
+        let ws_client_result = PubsubClient::new(CONFIG.wss_url.as_str()).await;
+
+        match ws_client_result {
+            Ok(ws_client) => {
+                if attempts == 1{
+                    println!("Successfully connected to WebSocket.");
+                }else{
+                    println!("Successfully connected to WebSocket after {} attempts.", attempts);
+                }
+                attempts = 0;
+                let (mut stream, _) = ws_client
+                    .logs_subscribe(
+                        RpcTransactionLogsFilter::Mentions(vec![CONFIG.raydium_lpv4.to_string()]),
+                        RpcTransactionLogsConfig {
+                            commitment: Some(CommitmentConfig::finalized()),
+                        },
+                    )
+                    .await?;
+
+                println!("Subscribed to Raydium Liquidity Pool.");
+
+                loop {
+                    match stream.next().await {
+                        Some(response) => {
+                            process_message(response).await;
+                        }
+                        None => {
+                            eprintln!("Stream closed, attempting to reconnect...");
+                            break;
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to connect to WebSocket. Attempt {} of 10. Error: {}", attempts, e);
+                if attempts >= 10 {
+                    eprintln!("Max reconnection attempts reached. Exiting...");
+                    return Err(anyhow::anyhow!("Max reconnection attempts reached").into());
+                }
+                sleep(Duration::from_secs(5)).await;
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
@@ -48,28 +101,7 @@ async fn main() -> Result<()> {
     logger.log(format!("Solana RPC http: {:?}", CONFIG.https_url.as_str()));
     logger.log(format!("Log instruction: {:?}", CONFIG.log_instruction.as_str()));
 
-    let ws_client = PubsubClient::new(CONFIG.wss_url.as_str()).await.expect("Failed to create WebSocket client. Please check your URL or network connection.");
-    let (mut stream, _) = match ws_client
-            .logs_subscribe(
-                RpcTransactionLogsFilter::Mentions(vec![CONFIG.raydium_lpv4.to_string()]),
-                RpcTransactionLogsConfig {
-                    commitment: Some(CommitmentConfig::finalized()),
-                },
-            )
-            .await
-        {
-            Ok(result) => result,
-            Err(err) => {
-                logger.log(format!("Failed to subscribe to logs: {}", err));
-                return Err(anyhow::anyhow!("Failed to subscribe to logs").into());
-            }
-        };
-    
-    logger.log("Subscribed to Raydium Liquidity Pool".to_string());
-    loop {
-        let response: Response<RpcLogsResponse> = stream.next().await.unwrap();
-        process_message(response).await;
-    }
+    start_subscriber().await
 }
 async fn process_message(response: Response<RpcLogsResponse>) {
     let value = response.value;
